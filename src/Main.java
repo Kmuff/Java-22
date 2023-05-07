8984c7bca9f0f8ec360aaa04809a0d50dd97ae65
Neto test <munkey@list.ru>
2023-05-07 11:08:06 +0300

    Origin master

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int check = 100;
+        int amount = 2000;
+
+        int bonus;
+        if (amount > 1000) {
+            bonus = amount / 100;
+        } else {
+            bonus = 0;
+        }
+        int total = check + amount + bonus;
+
+        System.out.println("Итоговый бонус:" + bonus);
+        System.out.println("Итоговая сумма: " + total);
+    }
+}
